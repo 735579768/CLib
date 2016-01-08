344cef242060d971b15c40fd85061ef88b5ee96e
kelicom <735579768@qq.com>
2016-01-08 10:49:40 +0800

    常用clib收藏

---
+++ CLib/CLib.cpp
@@ -0,0 +1,22 @@
+// CLib.cpp : 定义 DLL 应用程序的导出函数。
+//
+
+#include "stdafx.h"
+#include "CLib.h"
+
+
+// 这是导出变量的一个示例
+CLIB_API int nCLib=0;
+
+// 这是导出函数的一个示例。
+CLIB_API int fnCLib(void)
+{
+    return 42;
+}
+
+// 这是已导出类的构造函数。
+// 有关类定义的信息，请参阅 CLib.h
+CCLib::CCLib()
+{
+    return;
+}
